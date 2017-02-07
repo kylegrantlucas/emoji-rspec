5ec882e66ce85350020745397805dc1f810fcf7d
Kyle Lucas <kyle.lucas@iamplus.com>
2017-02-07 15:19:59 -0800

    Change aggressive thumbs

--- lib/emoji_test_love/formatters/aggressive_thumbs_formatter.rb
+++ lib/emoji_test_love/formatters/aggressive_thumbs_formatter.rb
@@ -7,15 +7,15 @@ module EmojiTestLove
     end
 
     def passed_display
-      "\u{1f44c} "
+      "\u{1f918} "
     end
 
     def failed_display
-      "\u{1f44a} "
+      "\u{1f595} "
     end
 
     def pending_display
-      "\u{270a} "
+      "\u{1f596} "
     end
   end
 end
